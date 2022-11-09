af8da525103322ebb794c2b434982adfa3eef564
drbobby27 <envioshseq@gmail.com>
2022-11-09 12:25:29 -0500

    Figure out DB connection

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ async function dbConnection()  {
         await db.authenticate()
         await db.sync({force:false})
         console.log('Connection has been established successfully.')
-        app.listen(5000) 
-        console.log(`Running on http://localhost:5000/api/v1/`)
+        app.listen(PORT) 
+        console.log(`Running on http://localhost:${PORT}/api/v1/`)
     } catch (error) {
         console.error('Unable to connect to the database:', error)
       }
